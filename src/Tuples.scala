9f3e1da7cd998b37837a6db0ac4fee6ece1ac451
Vikas Gupta <mevikasg@gmail.com>
2016-04-01 23:04:41 -0400

    Commited

--- src/Tuples.scala
+++ src/Tuples.scala
@@ -3,4 +3,14 @@
   */
 object Tuples {
 
+  def main(args: Array[String]){
+
+    val tuples = (123, "Vikas Gupta", 35, 123.123)
+
+    println(tuples.toString())
+
+    printf("%s Id is %d and his Age is %d. He ran %.2f miles so far.\n", tuples._2, tuples._1, tuples._3, tuples._4)
+
+    tuples.productIterator.foreach{ i => println(i)} }
+
 }
