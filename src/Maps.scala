42236eb478750f70b9cfd9fea5ba82bbb58f6830
Vikas Gupta <mevikasg@gmail.com>
2016-04-01 22:39:37 -0400

    Commited

--- src/Maps.scala
+++ src/Maps.scala
@@ -1,9 +1,33 @@
+import scala.collection.mutable
+import scala.reflect.internal.util.Collections
+
 /**
   * Created by vikasg on 3/31/2016.
   */
 object Maps {
 
   def main(args: Array[String]){
+    val empMap = Map("User" -> "mevikasg", "Name" -> "Vikas Gupta", "Age" -> "35")
+
+    if(empMap contains "User") {
+      printf("User: %s\n", empMap("User"))
+    }
+    println(empMap)
+    println(empMap.values)
+
+    //      empMap("User") = "vgupta"
+
+    val mutableMap = collection.mutable.Map("User" -> "mevikasg", "Name"-> "Vikas Gupta", "Age"-> "35")
+    println(mutableMap)
+
+    mutableMap("User") = "vgupta"
+
+    println(mutableMap)
+    println(mutableMap.values)
+
+    for((k,v) <- mutableMap)
+      printf("%s: %s\n", k,v)
+
 
 
   }
---
+++ src/Tuples.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by vikasg on 4/1/2016.
+  */
+object Tuples {
+
+}
